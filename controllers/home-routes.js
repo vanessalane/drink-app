86654c017a9301ca1b6dd64b88b6a98f7d728633
Vanessa Lane <vlane@23andme.com>
2020-08-29 11:44:42 -1000

    bigger hero for the homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.render('homepage', {
+        big_hero: true,
         nav_content_label: "recipes",
         hero_eyebrow: "it's time to",
         hero_title: "drink app"
