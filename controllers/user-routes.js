af3dcac77615a758d5f10119e7689a94a51c746f
Vanessa Lane <vlane@23andme.com>
2020-08-29 10:55:09 -1000

    add login route and file for template

--- controllers/user-routes.js
+++ controllers/user-routes.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 
+router.get('/login', (req, res) => {
+    res.render('login', {
+        hero_eyebrow: "log in"
+    });
+});
+
 router.get('/:id', (req, res) => {
     res.render('user', {
         hero_title: "author name"
