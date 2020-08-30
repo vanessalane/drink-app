8fa7b5695b0e6defd4357dda045f45f43e7ae834
Christian Vavuris <christianvavuris@Christians-MacBook-Pro.local>
2020-08-30 12:17:26 -0700

    added seed data in seeed.sql

---
+++ db/seed.sql
@@ -0,0 +1,30 @@
+
+
+INSERT INTO Users (username, password, email)
+VALUES ("test_user_1", "tester_1", "testytest1@gmail.com"),
+("test_user_2", "tester_2", "testytest2@gmail.com"),
+("test_user_3", "tester_3", "testytest3@gmail.com");
+
+
+INSERT INTO Ingredient (ingredient_name)
+VALUES ("Vodka"),
+("Gin"),
+("Bitters"),
+("Whiskey"),
+("Garnish"),
+("Tonic"),
+("Ice");
+
+INSERT INTO Recipe (recipe_name, instuctions, user_id, image_file_name,  rating)
+VALUES ("Gin and Tonic", 
+"Take 1 part Gin and 2 parts tonic.  Mix in a highball glass with ice.  Drink and Repeat.", 
+1, 
+"https://www.thespruceeats.com/thmb/9g5q0ChNcHs5IjThGwciEsa5d0Y=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/gin-tonic-5a8f334b8e1b6e0036a9631d.jpg", 
+4.3),
+("Vodka Shot", 
+"Just drink it. This isn't rocket science.", 
+2, 
+"https://img4.thelist.com/img/gallery/when-you-drink-vodka-every-night-this-is-what-happens-to-your-body/you-may-lose-weight-if-you-drink-vodka-every-night-1580240935.jpg", 
+2.2);
+
+
