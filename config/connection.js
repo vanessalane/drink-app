15b6421f6111db3f09ebc545059e2222981154f7
Christian Vavuris <christianvavuris@Christians-MacBook-Pro.local>
2020-08-30 11:24:13 -0700

    got the database all linked up.  now I need to make some seed data for the database, as the (NOT NULL) constraints are causing failures

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize('cocktail_db', process.env.DB_USER, process.env.DB_PW, {
+    sequelize = new Sequelize('drink_app', process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
         port: 3306
