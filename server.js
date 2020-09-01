f806174446b2ea021851e9e317b4cbd00e0bb2b5
Vanessa Lane <vlane@23andme.com>
2020-08-31 23:41:24 -1000

    add session to server

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
-const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+const path = require('path');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 
-// setup express server
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create();
+
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: { maxAge: 8 * 60 * 60 * 1000 },  // session expires after 8 hours
+  resave: false,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session(sess));
 app.use(express.static((path.join(__dirname, 'public'))));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-// set up express session
-const sess = {
-    secret: process.env.SECRET,
-    cookie: { maxAge: 8 * 60 * 60 * 1000 },  // session expires after 8 hours
-    resave: false,
-    rolling: true,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-};
-app.use(session(sess));
-
-// use handlebars
-const hbs = exphbs.create();
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 // turn on connection to db and server
 sequelize.sync({ force: true }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on port ${PORT}. Access locally at http://localhost:${PORT}`));
+    app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}`));
 });
