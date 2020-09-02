ad376b6ec923ac8e73c8015e7a062e2c304b5c72
Vanessa Lane <vlane@23andme.com>
2020-09-01 22:50:51 -1000

    comments in ingredient-routes.js

--- controllers/api/ingredient-routes.js
+++ controllers/api/ingredient-routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const { Post,  Ingredient, RecipeIngredient, Recipe} = require ('../../models');
+const { Ingredient, Recipe} = require ('../../models');
 
-//GET All Ingredient
-
+// GET All Ingredients
+// /api
 router.get('/',( reg, res) => {
     Ingredient.findAll({
         attributes:[
@@ -28,59 +28,49 @@ router.get('/',( reg, res) => {
         });
     })
 
-    // GET .api/ingredients/1
-    router.get('/:id',( req, res) => {
-        Ingredient.findOne({
-            // attributes:['ingredient_id'],
-            where: {
-                    ingredient_id: req.params.id
-            },
-            // include: [
-            //     {
-            //         model: Recipe,
-            //         attributes: ['recipe_id'],
-            //         as: 'ingredients',
-            //         through: {
-            //             attributes: ['amount'],
-            //             as: 'ingredient_amount'
-            //           }
-            //     },
-            // ],
-        })
-        .then(dbUserData => {
-            if(!dbUserData) {
-                res.status(404).json({ message: 'No user found with this id' });
-                return;
-            }
-            res.json(dbUserData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+// GET one ingredient by ingredient_id
+// /api/ingredients/1
+router.get('/:id',( req, res) => {
+    Ingredient.findOne({
+        where: {
+            ingredient_id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
     });
+});
 
 
-// looking for this object to pass in 
+// POST to add an ingredient
+// /api/ingredient
+// Required body:
 // {
 //     "ingredient_name": "orange peel"
 // }
-
-    router.post('/', (req, res) => {
-        Ingredient.create({
-            ingredient_name: req.body.ingredient_name,
-        })
-        .then(dbUserData => {
-            req.session.save(() => {
-              req.session.ingredient_id = dbUserData.ingredient_id;
-              req.session.ingredient_name = dbUserData.ingredient_name;
-              res.json(dbUserData);
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-       });
+router.post('/', (req, res) => {
+    Ingredient.create({
+        ingredient_name: req.body.ingredient_name,
+    })
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.ingredient_id = dbUserData.ingredient_id;
+            req.session.ingredient_name = dbUserData.ingredient_name;
+            res.json(dbUserData);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
     });
+});
 
 module.exports = router;
