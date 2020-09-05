2a90e3511412cc3cacc3af410567e8163443aeb9
Vanessa Lane <vlane@23andme.com>
2020-09-05 11:44:07 -1000

    move aws module into routes

--- controllers/api/aws-routes.js
+++ controllers/api/aws-routes.js
@@ -1,14 +1,75 @@
 const router = require('express').Router();
-const {uploadToAWS, deleteFromAWS} = require('../../utils/aws');
-
-router.post('/upload', async (req, res) => {
-    const fileName = req.body.filename
-    const uploadedFile = uploadToAWS(fileName, res);
-});
+const aws = require('aws-sdk');
+const fs = require('fs');
 
 router.post('/delete', async (req, res) => {
     const fileKey = req.body.filekey
-    const deletedFile = deleteFromAWS(fileKey, res);
+    // Set the config
+    aws.config.update({
+        region: process.env.S3_REGION,
+        credentials: {
+            accessKeyId: process.env.S3_ACCESS_KEY,
+            secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+        }
+    });
+
+    // create S3 service object
+    const s3 = new aws.S3();
+    
+    var params = {
+        Bucket: process.env.S3_BUCKET,
+        Key: fileKey,
+    };
+
+    s3.deleteObject(params, function (err, data) {
+        if (err) throw err;
+        if (data) {
+            res.json({
+                message: `File deleted from AWS`,
+                filekey: fileKey
+            })
+        }
+    });
+});
+
+router.post('/upload', async (req, res) => {
+    const fileName = req.body.filename
+    
+    // Set the config
+    aws.config.update({
+        region: process.env.S3_REGION,
+        credentials: {
+            accessKeyId: process.env.S3_ACCESS_KEY,
+            secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+        }
+    });
+    
+    // create S3 service object
+    const s3 = new aws.S3();
+
+    // read the file
+    fs.readFile(fileName, (err, data) => {
+
+        // pare down the image name
+        if (err) throw err;
+        const params = {
+            ACL: 'public-read',
+            Bucket: process.env.S3_BUCKET,
+            Key: `recipe_images/${fileName.substring(fileName.lastIndexOf('/') + 1)}`,
+            Body: data
+        };
+
+        // upload the file
+        s3.upload(params, function(s3Err, data) {
+            if (s3Err) throw s3Err
+            console.log(`File uploaded to AWS at ${data.Location}`)
+            res.json({
+                message: `File uploaded to AWS`,
+                image_url: data.Location,
+                image_file_name: data.Key
+            })
+        });
+    });
 });
 
 module.exports = router;
