71344b870d69ba439a964314cfaf55de51473689
Vanessa Lane <vlane@23andme.com>
2020-09-06 17:53:25 -1000

    delete image from s3 on recipe delete

--- controllers/api/recipe-routes.js
+++ controllers/api/recipe-routes.js
@@ -83,6 +83,7 @@ router.get('/:id', (req, res) => {
             'recipe_id',
             'recipe_name',
             'instructions',
+            'image_file_name',
             'image_url',
             [sequelize.literal(`(SELECT COUNT(*) FROM UserRecipeRating WHERE UserRecipeRating.recipe_id = Recipe.recipe_id)`), 'rating_count'],
             [sequelize.literal(`(SELECT AVG(rating) FROM UserRecipeRating WHERE UserRecipeRating.recipe_id = Recipe.recipe_id)`), 'rating'],
